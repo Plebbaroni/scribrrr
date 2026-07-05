50ffeaa0a0f832fc640a51d06806e6485693450f
Christopher Khim <z5695026@ad.unsw.edu.au>
2026-07-05 15:16:20 +1000

    new empty routes for messages and speakers

---
+++ apps/server/src/routes/speakers.ts
@@ -0,0 +1,5 @@
+// Speakers:
+// POST /sessions/sessionId:/speakers - creates a new speaker
+// GET /sessions/sessionId:/speakers - gets speakers given a particular session
+// PUT /sessions/sessionId:/speakers/:speakerId - renames a speaker
+// DELETE /sessions/sessionId:/speakers/:speakerId - situational, but needed to delete a speaker
